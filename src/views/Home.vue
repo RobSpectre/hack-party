262b56f8077da6d5dd789a07a4aad5ff8c5cae23
Rob Spectre <rob@brooklynhacker.com>
2022-09-11 18:23:10 -0400

    1) added base game content.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,64 @@
 Reveal
   .slides
     Title
+    PlayerBoard
+    DiffsquisiteCorpseRound(
+      :hints='maga_hints'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='maga_hints'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='maga_hints'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='maga_hints'
+    )
+    PlayerBoard
+    VideoSlide(
+      background='/video/voldemort.mp4'
+      :muted='false'
+    )
+    PlayerBoard
+    DiffsquisiteCorpseRound(
+      :hints='trek_hints'
+      :prize='2'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='trek_hints'
+      :prize='2'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='trek_hints'
+      :prize='2'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='trek_hints'
+      :prize='2'
+    )
+    PlayerBoard
+    VideoSlide(
+      background='/video/voldemort.mp4'
+      :muted='false'
+    )
+    PlayerBoard
+    DiffsquisiteCorpseRound(
+      :hints='service_hints'
+      :prize='3'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='service_hints'
+      :prize='3'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='service_hints'
+      :prize='3'
+    )
+    DiffsquisiteCorpseRound(
+      :hints='service_hints'
+      :prize='3'
+    )
+    PlayerBoard
 </template>
 
 <script>
@@ -15,6 +73,8 @@ import GameVideo from '@/components/base/GameVideo.vue'
 
 import PlayerBoard from '@/components/Players/PlayerBoard.vue'
 
+import DiffsquisiteCorpseRound from '@/components/DiffsquisiteCorpse/DiffsquisiteCorpseRound.vue'
+
 export default {
   name: 'Home',
   components: {
@@ -25,7 +85,20 @@ export default {
     BackgroundImageSlide,
     VideoSlide,
     ActionButton,
-    GameVideo
+    GameVideo,
+    DiffsquisiteCorpseRound
+  },
+  data () {
+    return {
+      maga_hints: null,
+      trek_hints: null,
+      service_hints: null
+    }
+  },
+  async mounted () {
+    this.maga_hints = await fetch('/data/maga.json').then(res => { return res.json() })
+    this.trek_hints = await fetch('/data/trek.json').then(res => { return res.json() })
+    this.service_hints = await fetch('/data/service.json').then(res => { return res.json() })
   }
 }
 </script>
