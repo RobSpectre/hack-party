a8c0cab61e0abeda4f80e7f35e3061f0b03ebf51
Rob Spectre <rob@brooklynhacker.com>
2022-10-01 14:50:55 -0400

    1) Added boss fights.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -49,6 +49,24 @@ Reveal
       :limit='2'
       :winnerIndex='0'
     )
+    VideoSlide(background='/video/bossfight.mp4')
+      h2.text-white.text-9xl.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    VideoSlide(background='/video/oligarchs.mp4')
+      h2.spooky Unused Explanations for an Oligarch's Death
+    Slide(class='darkgray')
+      h2.text-white.fragment Three mysterious explanations for a Russian oligarch's demise
+      h2.text-white.fragment Two are real - one are fake
+      h2.text-white.fragment Spot the fake for points
+      h2.text-white.fragment Contrarian bonus!
+      h1 Game Play
+    ItemGuess(
+      :items='oligarch_one'
+      :winnerIndex='0'
+      denomination=''
+      headerImage='/images/oligarchs.png'
+      question='Which is not a way an oligarch mysteriously died?'
+      :prize='3'
+    )
     PlayerBoard
     VideoSlide(
       background='/video/ultimate.mp4'
@@ -89,6 +107,18 @@ Reveal
       :limit='2'
       :winnerIndex='1'
       :prize='2'
+    )
+    VideoSlide(background='/video/bossfight.mp4')
+      h2.text-white.text-9xl.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    VideoSlide(background='/video/oligarchs.mp4')
+      h2.spooky Unused Explanations for an Oligarch's Death
+    ItemGuess(
+      :items='oligarch_two'
+      :winnerIndex='2'
+      denomination=''
+      headerImage='/images/oligarchs.png'
+      question='Which is not a way an oligarch mysteriously died?'
+      :prize='5'
     )
     PlayerBoard
     VideoSlide(
@@ -138,6 +168,18 @@ Reveal
       :winnerIndex='0'
       :prize='3'
     )
+    VideoSlide(background='/video/bossfight.mp4')
+      h2.text-white.text-9xl.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    VideoSlide(background='/video/oligarchs.mp4')
+      h2.spooky Unused Explanations for an Oligarch's Death
+    ItemGuess(
+      :items='oligarch_three'
+      :winnerIndex='1'
+      denomination=''
+      headerImage='/images/oligarchs.png'
+      question='Which is not a way an oligarch mysteriously died?'
+      :prize='10'
+    )
     PlayerBoard
 </template>
 
@@ -154,6 +196,8 @@ import PlayerBoard from '@/components/Players/PlayerBoard.vue'
 
 import VideoChoiceRound from '@/components/VideoChoice/VideoChoiceRound.vue'
 
+import ItemGuess from '@/components/WhichIsWhich/ItemGuess.vue'
+
 export default {
   name: 'Home',
   components: {
@@ -165,13 +209,29 @@ export default {
     VideoSlide,
     ActionButton,
     GameVideo,
-    VideoChoiceRound
+    VideoChoiceRound,
+    ItemGuess
   },
   data () {
     return {
       items: [
         { name: 'Cake', emoji: '🍰️', value: ' the video' },
         { name: 'Not Cake', emoji: '🪨️', value: ' the video' }
+      ],
+      oligarch_one: [
+        { name: 'Heart attack', emoji: '💓️', value: ' the excuse' },
+        { name: 'Hung by own robe off a handrail', emoji: '🪢️', value: ' the excuse' },
+        { name: 'Shot self multiple times in head', emoji: '🔫️', value: ' the excuse' }
+      ],
+      oligarch_two: [
+        { name: 'Suffocation from stroke', emoji: '🏌️', value: ' the excuse' },
+        { name: 'Fell from boat', emoji: '🚎️', value: ' the excuse' },
+        { name: 'Stepped in front of bus', emoji: '🚎️', value: ' the excuse' }
+      ],
+      oligarch_three: [
+        { name: 'Fell off a cliff', emoji: '🥾️', value: ' the excuse' },
+        { name: 'Private plane crash', emoji: '🛩️', value: ' the excuse' },
+        { name: 'Toad poison from shamanic ritual', emoji: '🐸️', value: ' the excuse' }
       ]
     }
   }
@@ -179,7 +239,7 @@ export default {
 </script>
 
 <style lang='scss'>
-@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Shrikhand&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Shrikhand&family=Creepster&display=swap');
 
 .nintendo {
   font-family: 'Press Start 2P', cursive !important;
@@ -189,6 +249,11 @@ export default {
 .title {
   font-family: 'Shrikhand', cursive !important;
   @apply mt-64 text-9xl text-white #{!important};
+}
+
+.spooky {
+  font-family: 'Creepster', cursive !important;
+  @apply mt-32 text-9xl text-red-600 #{!important};
 }
 
 html {
