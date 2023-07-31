151c8f2d9993d7f32a75e4ce769638de309db39e
Rob Spectre <rob@brooklynhacker.com>
2023-07-31 14:56:23 -0500

    Created content and interstitials.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,34 @@
 Reveal
   .slides
     Title
+    VideoSlide(background='/video/code.mp4')
+    VideoSlide(background='/video/code.mp4')
+      .title BadGPT
+    Slide
+      h2.fragment Solo
+      h2.fragment You will be presented with an AI model
+      h2.fragment It is trained to behave like a classic movie villain
+      h2.fragment Ask questions to learn more about who the model is emulating
+      h2.fragment Everyone can ask as many questions as they want...
+      h2.fragment ...but each question can only be asked once
+      h2.fragment To win, ask the model by name
+      h1 Game Play
+    PlayerBoard
+    Scoreboard
     VideoSlide(background='/video/bossfight.mp4')
       h2.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    Slide(class='blue')
+      h2.text-white GO FIND TROLLS!
+      AudioPlayer(src='/sounds/baby_shark_metal.mp3')
     PlayerBoard
-    VideoSlide(
-      background='/video/'
-      :muted='false'
-    )
-    PlayerBoard
+    Scoreboard
     VideoSlide(background='/video/bossfight.mp4')
       h2.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    Slide(class='blue')
+      h2.text-white Get a grown up to count how many times you can jump to this song.
+      AudioPlayer(src='/sounds/jump_around_clean.mp3')
     PlayerBoard
-    VideoSlide(
-      background='/video/'
-      :muted='false'
-    )
-    PlayerBoard
-    VideoSlide(background='/video/bossfight.mp4')
-      h2.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    Scoreboard
     PlayerBoard
 </template>
 
@@ -34,6 +44,10 @@ import GameVideo from '@/components/base/GameVideo.vue'
 
 import PlayerBoard from '@/components/Players/PlayerBoard.vue'
 
+import Scoreboard from '@/components/Scoreboard/Scoreboard.vue'
+
+import AudioPlayer from '@/components/AuctionEar/AudioPlayer.vue'
+
 export default {
   name: 'Home',
   components: {
@@ -44,17 +58,24 @@ export default {
     BackgroundImageSlide,
     VideoSlide,
     ActionButton,
-    GameVideo
+    GameVideo,
+    Scoreboard,
+    AudioPlayer
   }
 }
 </script>
 
 <style lang='scss'>
-@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Tektur&display=swap');
 
 .nintendo {
   font-family: 'Press Start 2P', cursive !important;
-  @apply mt-72 text-9xl text-white #{!important};
+  @apply mt-32 text-9xl text-white #{!important};
+}
+
+.title {
+  font-family: 'Tektur', cursive !important;
+  @apply mt-32 text-9xl  text-white #{!important};
 }
 
 html {
