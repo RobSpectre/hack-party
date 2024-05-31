cf4d48a9025f2c71826edbfc22c20d0fe5b681fd
Rob Spectre <rob@brooklynhacker.com>
2024-05-31 11:37:06 -0400

    1) Added boss fights and interstitials.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,75 @@
 Reveal
   .slides
     Title
+    BackgroundImageSlide(background='/images/famous_dead_writers.webp')
+    Slide
+      h2.fragment Solo
+      h2.fragment Interact with a GPT trained to emulate a famous dead writer
+      h2.fragment Ask questions of the writer at will
+      h2.fragment First to guess by name wins
+      h2.fragment We'll adjust difficulty each round
+      h1 Game Play
     PlayerBoard
+    Scoreboard
     VideoSlide(background='/video/bossfight.mp4')
       h2.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    CameoPriceIsRightRound(
+      v-for='figure in actionFigures.slice(0, 3)'
+      :key='figure.name'
+      :celebrity='figure.name'
+      :hint1='figure.year.toString()'
+      :hint2='figure.name'
+      hint3=''
+      video=''
+      :image='figure.image'
+      :price='figure.price'
+      question='How much did this action figure fetch?'
+      :prize='3'
+    )
     PlayerBoard
     VideoSlide(
-      background='/video/'
+      background='/video/action_league.mp4'
       :muted='false'
     )
     PlayerBoard
+    Scoreboard(:prize='2')
     VideoSlide(background='/video/bossfight.mp4')
       h2.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    CameoPriceIsRightRound(
+      v-for='figure in actionFigures.slice(3, 6)'
+      :key='figure.name'
+      :celebrity='figure.name'
+      :hint1='figure.year.toString()'
+      :hint2='figure.name'
+      hint3=''
+      video=''
+      :image='figure.image'
+      :price='figure.price'
+      question='How much did this action figure fetch?'
+      :prize='5'
+    )
     PlayerBoard
     VideoSlide(
-      background='/video/'
+      background='/video/prometheus_and_bob.mp4'
       :muted='false'
     )
     PlayerBoard
+    Scoreboard(:prize='3')
     VideoSlide(background='/video/bossfight.mp4')
       h2.nintendo(style='margin-top: 24rem;') Boss Fight!!
+    CameoPriceIsRightRound(
+      v-for='figure in actionFigures.slice(6, 10)'
+      :key='figure.name'
+      :celebrity='figure.name'
+      :hint1='figure.year.toString()'
+      :hint2='figure.name'
+      hint3=''
+      video=''
+      :image='figure.image'
+      :price='figure.price'
+      question='How much did this action figure fetch?'
+      :prize='10'
+    )
     PlayerBoard
 </template>
 
@@ -35,6 +85,12 @@ import GameVideo from '@/components/base/GameVideo.vue'
 
 import PlayerBoard from '@/components/Players/PlayerBoard.vue'
 
+import Scoreboard from '@/components/Scoreboard/Scoreboard.vue'
+
+import CameoPriceIsRightRound from '@/components/CameoPriceIsRight/CameoPriceIsRightRound.vue'
+
+import actionFigures from '@/assets/most_expensive_action_figures.json'
+
 export default {
   name: 'Home',
   components: {
@@ -45,7 +101,14 @@ export default {
     BackgroundImageSlide,
     VideoSlide,
     ActionButton,
-    GameVideo
+    GameVideo,
+    Scoreboard,
+    CameoPriceIsRightRound
+  },
+  data () {
+    return {
+      actionFigures: actionFigures
+    }
   }
 }
 </script>
